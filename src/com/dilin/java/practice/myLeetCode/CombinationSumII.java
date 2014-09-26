6b4eaaa0c1b59d7c1c5ab4fd641fbbc354b9fa6a
dilin <dl2869@columbia.edu>
2014-09-26 15:22:39 -0400

    https://oj.leetcode.com/problems/combination-sum-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/CombinationSumII.java
@@ -0,0 +1,27 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumII {
+	public List<List<Integer>> combinationSum2(int[] num, int target) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res, num, 0, target, new ArrayList<Integer>());
+        return res;
+    }
+    private void helper (List<List<Integer>> res, int[] num, int start, int target, List<Integer> temp) {
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(temp));
+        } else {
+            for (int i = start; i < num.length && target >= num[i]; i++) {
+                if (i == start || num[i] != num[i - 1]) {
+                    temp.add(num[i]);
+                    helper (res, num, i + 1, target - num[i], temp);
+                    temp.remove(temp.size() - 1);
+                }
+            }
+        }
+    }
+}
