f700785d96f2cf25e1988e36ec6ebea9fb66e8f1
dilin <dl2869@columbia.edu>
2014-09-28 21:36:31 -0400

    https://oj.leetcode.com/problems/reverse-nodes-in-k-group/

---
+++ src/com/dilin/java/practice/myLeetCode/ReverseNodesInK.java
@@ -0,0 +1,39 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class ReverseNodesInK {
+	public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        ListNode dummyScan = dummy;
+        dummy.next = head;
+        while (head != null) {
+            int i = 0;
+            for (; i < k; i++) {
+                if (head == null) {
+                    break;
+                }
+                head = head.next;
+            }
+            if (i == k) {
+                ListNode newNode = head, scan = dummyScan.next, tail = scan;
+                while (scan != head) {
+                    ListNode temp = scan.next;
+                    scan.next = newNode;
+                    newNode = scan;
+                    scan = temp;
+                }
+                dummyScan.next = newNode;
+                dummyScan = tail;
+            }
+        }
+        return dummy.next;
+    }
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+}
