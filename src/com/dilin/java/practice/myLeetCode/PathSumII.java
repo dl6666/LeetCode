fa1a2523e786657cd9ee322050e94d9744be2a8c
dilin <dl2869@columbia.edu>
2014-09-30 16:29:47 -0400

    https://oj.leetcode.com/problems/path-sum-ii/  https://oj.leetcode.com/problems/valid-number/

---
+++ src/com/dilin/java/practice/myLeetCode/PathSumII.java
@@ -0,0 +1,35 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dilin.java.practice.imp.problems.MaxTree.TreeNode;
+
+public class PathSumII {
+	public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return res;
+        }
+        helper(root, sum, new ArrayList<Integer>(), res);
+        return res;
+    }
+    private void helper (TreeNode root, int sum, List<Integer> temp, List<List<Integer>> res) {
+        if (sum == root.val && root.left == null && root.right == null) {
+            temp.add(root.val);
+            res.add(new ArrayList<Integer>(temp));
+            temp.remove(temp.size() - 1);
+        } else {
+            if (root.left != null) {
+                temp.add(root.val);
+                helper(root.left, sum - root.val, temp, res);
+                temp.remove(temp.size() - 1);
+            }
+            if (root.right != null) {
+                temp.add(root.val);
+                helper(root.right, sum - root.val, temp, res);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
---
+++ src/com/dilin/java/practice/myLeetCode/ValidNumber.java
@@ -0,0 +1,51 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class ValidNumber {
+	public enum Input {
+		INVALID(-1),
+		DOT(0),
+		SPACE(1),
+		DIGIT(2),
+		SIGN(3),
+		E(4);
+		public int val;
+		private Input (int input) {
+			this.val = input;
+		}
+	};
+	public boolean isNumber(String s) {
+		int[][] trans  = new int[][] {
+		        { -1, -1, -1, -1},
+				{ 4, 1, 3, 2,-1},
+				{ 4,-1, 3,-1,-1},
+				{ 5, 6, 3,-1, 7},
+				{-1,-1, 5,-1,-1},
+				{-1, 6, 5,-1, 7},
+				{-1, 6,-1,-1,-1},
+				{-1,-1, 8, 9,-1},
+				{-1, 6, 8,-1,-1},
+				{-1,-1, 8,-1,-1}
+		};
+		int len = s.length();
+		int state = 1;
+		for (int i = 0; i < len; i++) {
+		    Input input = Input.INVALID;
+			char curt = s.charAt(i);
+			if (curt == '.') {
+				input = Input.DOT;
+			} else if (curt == ' ') {
+				input = Input.SPACE;
+			} else if (curt == '+' || curt == '-') {
+				input = Input.SIGN;
+			} else if (curt == 'e' || curt == 'E') {
+				input = Input.E;
+			} else if (curt >= '0' && curt <= '9') {
+				input = Input.DIGIT;
+			}
+			if (input.val == -1) return false;
+			state = trans[state][input.val];
+			if (state == -1) return false;
+		}
+		return state == 3 || state == 5 || state == 6 || state == 8;
+	}
+}
