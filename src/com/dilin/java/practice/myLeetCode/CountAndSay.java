5b7c2d4f03340b02e6639ee8a6478ddfe4f0b4b2
dl6666 <dl2869@columbia.edu>
2014-10-03 17:31:46 -0400

    https://oj.leetcode.com/problems/count-and-say/

---
+++ src/com/dilin/java/practice/myLeetCode/CountAndSay.java
@@ -0,0 +1,25 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class CountAndSay {
+
+	public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuffer sb = new StringBuffer();
+            int j = 0, len = s.length();
+            while (j < len) {
+                char curt = s.charAt(j);
+                int count = 1;
+                while (j + 1 < len && s.charAt(j + 1) == curt) {
+                    count++;
+                    j++;
+                }
+                sb.append(count);
+                sb.append(curt);
+                j++;
+            }
+            s = sb.toString();
+        }
+        return s;
+    }
+}
