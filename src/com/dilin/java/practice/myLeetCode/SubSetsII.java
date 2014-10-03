d6d25f29b0e4bc4c639812b38a400fc0b3b64ab2
dl6666 <dl2869@columbia.edu>
2014-10-03 15:38:08 -0400

    https://oj.leetcode.com/problems/subsets-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/SubSetsII.java
@@ -0,0 +1,26 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubSetsII {
+
+	public List<List<Integer>> subsetsWithDup(int[] num) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(num);
+        helper(res, num, 0, new ArrayList<Integer>());
+        return res;
+    }
+    private void helper (List<List<Integer>> res, int[] num, int index, List<Integer> temp) {
+        int len = num.length;
+        res.add(new ArrayList<Integer>(temp));
+        for (int i = index; i < len; i++) {
+            if (i == index || num[i] != num[i - 1]) {
+                temp.add(num[i]);
+                helper(res, num, i + 1, temp);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
