07c92a53118af312701160f0b4bb0da49c1e0dea
dl6666 <dl2869@columbia.edu>
2014-10-02 23:22:00 -0400

    https://oj.leetcode.com/problems/word-ladder/

---
+++ src/com/dilin/java/practice/myLeetCode/WordLadder.java
@@ -0,0 +1,43 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class WordLadder {
+	public int ladderLength(String start, String end, Set<String> dict) {
+        Queue<String> q = new LinkedList<String>();
+        int count = 1;
+        if (dict.contains(start)) {
+            if (start.equals(end)) return count;
+            q.offer(start);
+            dict.remove(start);
+        }
+        while (!q.isEmpty()) {
+            int size = q.size();
+            count++;
+            for (int j = 0; j < size; j++) {
+                char[] curt = q.poll().toCharArray();
+                int len = curt.length;
+                for (int i = 0; i < len; i++) {
+                    char save = curt[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        curt[i] = c;
+                        String changed = String.valueOf(curt);
+                        if (dict.contains(changed)) {
+                            if (changed.equals(end)){
+                                return count;
+                            }
+                            dict.remove(changed);
+                            q.offer(changed);
+                        }
+                    }
+                    curt[i] = save;
+                }
+            }
+        }
+        
+        return 0;
+    }
+
+}
--- src/com/dilin/java/practice/myLeetCode/WordSearch.java
+++ src/com/dilin/java/practice/myLeetCode/WordSearch.java
@@ -11,11 +11,9 @@ public class WordSearch {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if ( board[i][j] == word.charAt(0) ) {
-                    visited[i][j] = true;
                     if (helper (board, i, j, word, 0, visited)) {
                         return true;
                     }
-                    visited[i][j] = false;
                 }
             }
         }
@@ -23,38 +21,15 @@ public class WordSearch {
     }
     private boolean helper (char[][] board, int i, int j, String word, int index, boolean[][] visited) {
         int len  = word.length(), row = board.length, col = board[0].length;
-        if (index == len - 1) {
+        if (index == len) {
             return true;
         }
-        if (i > 0 && !visited[i - 1][j] && word.charAt(index + 1) == board[i - 1][j]) {
-            visited[i - 1][j] = true;
-            if (helper (board, i - 1, j, word, index + 1, visited)) {
-                return true;
-            }
-            visited[i - 1][j] = false;
+        if (i < 0 || i >= row || j >= col || j < 0 || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
         }
-        if (j > 0 && !visited[i][j - 1] && word.charAt(index + 1) == board[i][j - 1]) {
-            visited[i][j - 1] = true;
-            if (helper (board, i, j - 1, word, index + 1, visited)) {
-                return true;
-            }
-            visited[i][j - 1] = false;
-        }
-        if (i < row - 1 && !visited[i + 1][j] && word.charAt(index + 1) == board[i + 1][j]) {
-            visited[i + 1][j] = true;
-            if (helper (board, i + 1, j, word, index + 1, visited)) {
-                return true;
-            }
-            visited[i + 1][j] = false;
-        }
-        if (j < col - 1 && !visited[i][j + 1] && word.charAt(index + 1) == board[i][j + 1]) {
-            visited[i][j + 1] = true;
-            if (helper (board, i, j + 1, word, index + 1, visited)) {
-                return true;
-            }
-            visited[i][j + 1] = false;
-        }
-    
-        return false;
+        visited[i][j] = true;
+        boolean  res = helper (board, i - 1, j, word, index + 1, visited) || helper (board, i + 1, j, word, index + 1, visited) || helper (board, i, j - 1, word, index + 1, visited) || helper (board, i, j + 1, word, index + 1, visited);
+        visited[i][j] = false;
+        return res;
     }
 }
