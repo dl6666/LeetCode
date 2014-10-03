0ee77594d781bdb60c88fa61ded8edc1fbbcd868
dl6666 <dl2869@columbia.edu>
2014-10-03 17:12:29 -0400

    https://oj.leetcode.com/submissions/detail/12459012/

---
+++ src/com/dilin/java/practice/myLeetCode/UniqueBinarySearchTreeII.java
@@ -0,0 +1,45 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTreeII {
+	public List<TreeNode> generateTrees(int n) {
+        return helper(1,n);
+    }
+    public List<TreeNode> helper (int start, int end) {
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = helper(start, i - 1);
+            List<TreeNode> right = helper(i + 1, end);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = cloneTree(l);
+                    root.right = cloneTree(r);
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+    public TreeNode cloneTree (TreeNode root) {
+        if (root == null) return null;
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = cloneTree(root.left);
+        newRoot.right = cloneTree(root.right);
+        return newRoot;
+    }
+    public class TreeNode{
+		public int val = 0;
+		public TreeNode left = null;
+		public TreeNode right = null;
+		public TreeNode (int val) {
+			this.val = val;
+		}
+	}
+}
