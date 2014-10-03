f186a360ada5d776119c49e089e652f80be13d07
dl6666 <dl2869@columbia.edu>
2014-10-03 17:41:03 -0400

    https://oj.leetcode.com/problems/unique-paths-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/UniquePathII.java
@@ -0,0 +1,29 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class UniquePathII {
+	
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null) return 0;
+        int row = obstacleGrid.length, col = obstacleGrid[0].length;
+        int[][] dp = new int[row][col];
+        if (obstacleGrid[0][0] == 0) {
+            dp[0][0] = 1;
+        }
+        for (int i = 1; i < row && dp[i - 1][0] == 1 && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j < col && dp[0][j - 1] == 1 && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+}
