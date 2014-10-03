0b59c5b8354a0638f5fa7730491fd0207c5e7363
dl6666 <dl2869@columbia.edu>
2014-10-03 16:42:17 -0400

    https://oj.leetcode.com/problems/jump-game/

---
+++ src/com/dilin/java/practice/myLeetCode/JumpGame.java
@@ -0,0 +1,13 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class JumpGame {
+
+	public boolean canJump(int[] A) {
+        int reach = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i > reach) return false;
+            reach = Math.max(reach, i + A[i]);
+        }
+        return true;
+    }
+}
