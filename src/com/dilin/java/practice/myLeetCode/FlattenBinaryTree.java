e8ed13aa1fe5cc1d97763ce6e6510d8eef8d2171
dl6666 <dl2869@columbia.edu>
2014-10-03 23:03:04 -0400

    https://oj.leetcode.com/problems/flatten-binary-tree-to-linked-list/

---
+++ src/com/dilin/java/practice/myLeetCode/FlattenBinaryTree.java
@@ -0,0 +1,32 @@
+package com.dilin.java.practice.myLeetCode;
+
+
+public class FlattenBinaryTree {
+
+	public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode leftBiggest = getBig(root.left);
+        if (leftBiggest != null) {
+            leftBiggest.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+    }
+    public TreeNode getBig(TreeNode root) {
+        if (root == null) return null;
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root;
+    }
+    public class TreeNode{
+		public int val = 0;
+		public TreeNode left = null;
+		public TreeNode right = null;
+		public TreeNode (int val) {
+			this.val = val;
+		}
+	}
+}
