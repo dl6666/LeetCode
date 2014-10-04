cdf29925474cba0b39037e07bbaf5bf92db51110
dl6666 <dl2869@columbia.edu>
2014-10-04 00:43:03 -0400

    https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ src/com/dilin/java/practice/myLeetCode/RemoveKthNode.java
@@ -0,0 +1,27 @@
+package com.dilin.java.practice.myLeetCode;
+
+
+public class RemoveKthNode {
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0), pre = dummy;
+        dummy.next = head;
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+        while (head != null) {
+            pre = pre.next;
+            head = head.next;
+        }
+        pre.next = pre.next.next;
+        return dummy.next;
+    }
+	public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+        	val = x;
+    	    next = null;
+       }
+    }
+}
