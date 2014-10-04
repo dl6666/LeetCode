58ae182ed9a7b628ad71d290cbfba2fa6b7bdeed
dilin <dl2869@columbia.edu>
2014-10-04 18:58:30 -0400

    https://oj.leetcode.com/problems/permutations/

---
+++ src/com/dilin/java/practice/myLeetCode/Permutations.java
@@ -0,0 +1,27 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+	public List<List<Integer>> permute(int[] num) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res, num, new boolean[num.length], new ArrayList<Integer>());
+        return res;
+    }
+    public void helper(List<List<Integer>> res, int[] num, boolean[] visited, List<Integer> temp) {
+        if (temp.size() == num.length) {
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for (int i = 0; i < num.length; i++) {
+            if (!visited[i]) {
+                temp.add(num[i]);
+                visited[i] = true;
+                helper(res, num, visited, temp);
+                temp.remove(temp.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
