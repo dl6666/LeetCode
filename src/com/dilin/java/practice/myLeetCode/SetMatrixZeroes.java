51feb20ca5fb8181e18468f9b49d7873a437ff86
dilin <dl2869@columbia.edu>
2014-10-04 15:30:35 -0400

    https://oj.leetcode.com/problems/set-matrix-zeroes/

---
+++ src/com/dilin/java/practice/myLeetCode/SetMatrixZeroes.java
@@ -0,0 +1,31 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class SetMatrixZeroes {
+
+	public void setZeroes(int[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0)return;
+        int row = matrix.length, col = matrix[0].length;
+        boolean[] r = new boolean[row], c = new boolean[col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    r[i] = c[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            if (r[i]) {
+                for (int j = 0; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < col; j++) {
+            if (c[j]) {
+                for (int i = 0; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
