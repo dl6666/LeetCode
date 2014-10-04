7c294a11e95488835a12dea1c226cd87ba4e7196
dilin <dl2869@columbia.edu>
2014-10-04 14:47:57 -0400

    https://oj.leetcode.com/problems/spiral-matrix-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/SpircalMatrix.java
@@ -0,0 +1,19 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class SpircalMatrix {
+
+	public int[][] generateMatrix(int n) {
+        int level = n / 2, num = 1;
+        int[][] res = new int[n][n];
+        for (int i = 0; i < level; i++) {
+            for (int j = i; j < n - i - 1; j++) res[i][j] = num++;
+            for (int j = i; j < n - i - 1; j++) res[j][n - 1 - i] = num++;
+            for (int j = i; j < n - i - 1; j++) res[n - 1 - i][n - 1 - j] = num++;
+            for (int j = i; j < n - i - 1; j++) res[n - 1 - j][i] = num++;
+        }
+        if (n % 2 != 0) {
+            res[n / 2][n / 2] = num;
+        }
+        return res;
+    }
+}
