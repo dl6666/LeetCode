25d45570af97ffe5d372b962c42fd551a0febb16
dl6666 <dl2869@columbia.edu>
2014-10-04 01:25:07 -0400

    https://oj.leetcode.com/problems/combinations/

---
+++ src/com/dilin/java/practice/myLeetCode/Combinations.java
@@ -0,0 +1,24 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+
+	public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res, n, k, 1, new ArrayList<Integer>());
+        return res;
+    }
+    public void helper(List<List<Integer>> res, int n, int k, int index, List<Integer> temp) {
+        if (temp.size() == k) {
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for (int i = index; i <= n; i++) {
+            temp.add(i);
+            helper(res, n, k, i + 1, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
