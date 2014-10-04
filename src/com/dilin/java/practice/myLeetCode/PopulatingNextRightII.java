32a612ee06772d977f17ff667f5d59d4d9bc8631
dilin <dl2869@columbia.edu>
2014-10-04 14:08:05 -0400

    https://oj.leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/PopulatingNextRightII.java
@@ -0,0 +1,34 @@
+package com.dilin.java.practice.myLeetCode;
+
+
+public class PopulatingNextRightII {
+
+	public void connect(TreeLinkNode root) {
+        TreeLinkNode pre = root, nextDummy = new TreeLinkNode(0), next = nextDummy;
+        while(pre != null) {
+            while (pre != null) {
+                if (pre.left != null) {
+                    next.next = pre.left;
+                    next = next.next;
+                }
+                if (pre.right != null) {
+                    next.next = pre.right;
+                    next = next.next;
+                }
+                pre = pre.next;
+            }
+            pre = nextDummy.next;
+            nextDummy.next = null;
+            next = nextDummy;
+        }
+    }
+	class TreeLinkNode {
+	      TreeLinkNode left;
+	      TreeLinkNode right;
+	      TreeLinkNode next;
+	      int val;
+	      TreeLinkNode(int val) {
+	    	  this.val = val;
+	      }
+	}
+}
