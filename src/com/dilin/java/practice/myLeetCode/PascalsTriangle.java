9fe3c10474c3493f70f18b8a50fce94ba031f41a
dilin <dl2869@columbia.edu>
2014-10-04 14:27:20 -0400

    https://oj.leetcode.com/problems/pascals-triangle-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/PascalsTriangle.java
@@ -0,0 +1,24 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<Integer>(), curt = new ArrayList<Integer>();
+        for (int i = 0; i <= rowIndex; i++) {
+            curt = new ArrayList<Integer>();//critical
+            curt.add(1);
+            if (i > 0) {
+                int size = pre.size();
+                for (int j = 1; j < size; j++) {
+                    curt.add(pre.get(j - 1) + pre.get(j));
+                }
+                curt.add(1);
+            }
+            pre = curt;
+        }
+        return curt;
+    }
+}
