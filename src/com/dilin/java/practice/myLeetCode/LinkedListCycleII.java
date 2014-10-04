8798c405f6a94250816cc4a39a8ca70f6c407370
dilin <dl2869@columbia.edu>
2014-10-04 14:33:10 -0400

    https://oj.leetcode.com/problems/linked-list-cycle-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/LinkedListCycleII.java
@@ -0,0 +1,34 @@
+package com.dilin.java.practice.myLeetCode;
+
+
+public class LinkedListCycleII {
+
+	public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+        if (!cycle) return null;
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+	public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+        	val = x;
+    	    next = null;
+       }
+    }
+}
+
