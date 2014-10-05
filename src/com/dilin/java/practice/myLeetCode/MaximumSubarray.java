ecd730fc43815123cffef4d75e213365d55f5071
dl6666 <dl2869@columbia.edu>
2014-10-05 16:41:45 -0400

    https://oj.leetcode.com/problems/maximum-subarray/

---
+++ src/com/dilin/java/practice/myLeetCode/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class MaximumSubarray {
+
+	public int maxSubArray(int[] A) {
+        int sum = 0, len = A.length, max = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += A[i];
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
