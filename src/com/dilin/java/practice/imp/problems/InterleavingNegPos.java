b3f6687a389d6318ff028ed61fd7f35602f66344
dl6666 <dl2869@columbia.edu>
2014-10-06 00:21:35 -0400

    interleaving neg and pos numbers

---
+++ src/com/dilin/java/practice/imp/problems/InterleavingNegPos.java
@@ -0,0 +1,31 @@
+package com.dilin.java.practice.imp.problems;
+
+public class InterleavingNegPos {
+
+	public int[] reorder(int[] num) {
+		if (num.length < 2) return num;
+		int len = num.length;
+		for (int i = 1; i < len - 1; i++) {
+			int index = i + 1;
+			if (num[index] * num[i] < 0) {
+				continue;
+			}
+			while (index < len && num[index] * num[i] > 0) {
+				index++;
+			}
+			if (index < len) {
+				swap (num, i + 1, index);
+				i++;
+			} else {
+				i = len;
+			}
+		}
+		return num;
+	}
+	private void swap(int[] num, int i, int j) {
+		if (i == j) return;
+		int temp = num[i];
+		num[i] = num[j];
+		num[j] = temp;
+	}
+}
---
+++ src/com/dilin/java/practice/imp/problems/InterleavingNegPosDriver.java
@@ -0,0 +1,16 @@
+package com.dilin.java.practice.imp.problems;
+
+public class InterleavingNegPosDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterleavingNegPos it = new InterleavingNegPos();
+		int[] input = {-2,1,1,1,4,-3,-5,2};
+		it.reorder(input);
+		for (int i : input) {
+			System.out.print(i);
+
+		}
+	}
+
+}
