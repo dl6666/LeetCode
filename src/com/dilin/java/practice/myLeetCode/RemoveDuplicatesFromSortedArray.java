691d197451ad0ef736930a4cb6eed4667ec13f56
dilin <dl2869@columbia.edu>
2014-10-09 14:45:31 -0400

    https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ src/com/dilin/java/practice/myLeetCode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class RemoveDuplicatesFromSortedArray {
+
+	public int removeDuplicates(int[] A) {
+        if (A == null || A.length == 0) return 0;
+        int end = 1, len = A.length;
+        for (int i = 1; i < len; i++) {
+            if (A[i] != A[i - 1]) {
+                A[end] = A[i];
+                end++;
+            }
+        }
+        return end;
+    }
+}
