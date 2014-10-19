4c3361d172587bd34ee0f98ba201cff19cc2ee56
dilin <dl2869@columbia.edu>
2014-10-19 19:49:47 -0400

    https://oj.leetcode.com/problems/gas-station/“

---
+++ src/com/dilin/java/practice/imp/problems/AtMostTwoChar.java
@@ -0,0 +1,30 @@
+package com.dilin.java.practice.imp.problems;
+
+
+public class AtMostTwoChar {
+
+	public static void main (String[] args) {
+		System.out.println(longest("fjdlksajjfjjjjjjddddkkkkief"));
+	}
+	public static int longest(String s) {
+		char first = ' ', second = ' ';
+		int firstS = 0, firstE = 0, secondS = 0, secondE = 0, len = s.length(), max = 0;
+		for (int i = 0; i < len; i++) {
+			char curt = s.charAt(i);
+			if (curt == first) {
+				firstE = i;
+			} else if (curt == second) {
+				secondE = i;
+			} else {
+				first = second;
+				second = curt;
+				firstS = secondS;
+				firstE = secondE;
+				secondS = i;
+				secondE = i;
+			}
+			max = Math.max(max, secondE - firstS + 1);
+		}
+		return max;
+	}
+}
---
+++ src/com/dilin/java/practice/myLeetCode/GasStation.java
@@ -0,0 +1,19 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class GasStation {
+
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || gas.length == 0 || cost == null || cost.length == 0 || gas.length != cost.length) return -1;
+        int sum = 0, index = 0, rem = 0, len = gas.length;
+        for (int i = 0; i < len; i++) {
+            if (rem < 0) {
+                index = i;
+                rem = 0;
+            }
+            rem += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+        }
+        if (sum < 0) return -1;
+        else return index;
+    }
+}
