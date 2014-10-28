204a833ea651e53cfefbe3f3d3b4c7985f111fc3
dilin <dl2869@columbia.edu>
2014-10-28 13:57:38 -0400

    https://oj.leetcode.com/problems/permutations-ii/

---
+++ src/com/dilin/java/practice/myLeetCode/PermutationsII.java
@@ -0,0 +1,28 @@
+package com.dilin.java.practice.myLeetCode;
+
+public class PermutationsII {
+
+	public List<List<Integer>> permuteUnique(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(num, new boolean[num.length], new ArrayList<Integer>(), res);
+        return res;
+    }
+    private void helper(int[] num, boolean[] used, List<Integer> temp, List<List<Integer>> res) {
+        if (temp.size() == num.length) {
+            res.add(new ArrayList<Integer>(temp));
+        } else {
+            int pre = Integer.MAX_VALUE;
+            for (int i = 0; i < num.length; i++) {
+                if (!used[i] && num[i] != pre) {
+                    temp.add(num[i]);
+                    used[i] = true;
+                    helper(num, used, temp, res);
+                    used[i] = false;
+                    temp.remove(temp.size() - 1);
+                    pre = num[i];
+                }
+            }
+        }
+    }
+}
