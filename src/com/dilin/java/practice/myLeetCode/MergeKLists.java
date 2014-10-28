58b8d99e54899860a4b60973620dcda63124800e
dilin <dl2869@columbia.edu>
2014-10-28 13:40:51 -0400

    https://oj.leetcode.com/problems/merge-k-sorted-lists/

---
+++ src/com/dilin/java/practice/myLeetCode/MergeKLists.java
@@ -0,0 +1,37 @@
+package com.dilin.java.practice.myLeetCode;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MergeKLists {
+
+	public ListNode mergeKLists(List<ListNode> lists) {
+		if (lists == null || lists.size() == 0) return null;
+        int size = lists.size();
+        ListNode dummy = new ListNode(0),save = dummy;
+    	Comparator<ListNode> comp = new Comparator<ListNode>(){
+    	    public int compare(ListNode l1, ListNode l2) {
+    	        return l1.val - l2.val;
+    	    }
+    	};
+    	PriorityQueue<ListNode> que = new PriorityQueue<ListNode>(size, comp);
+    	Iterator<ListNode> it = lists.iterator();
+    	while (it.hasNext()) {
+    	    ListNode curt = it.next();
+    	    if (curt != null) {
+    	        que.offer(curt);
+    	    }
+    	}
+    	while (!que.isEmpty()) {
+    	    ListNode curt = que.poll();
+    	    dummy.next = curt;
+    	    dummy = dummy.next;
+    	    if (curt.next != null) {
+    	        que.offer(curt.next);
+    	    }
+    	}
+    	return save.next;
+    }
+}
