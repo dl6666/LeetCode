29b30e828efaa36e1238a7e207f01a9aba50479c
dilin <dl2869@columbia.edu>
2014-10-28 22:59:04 -0400

    multiply strings

--- src/com/dilin/java/practice/myLeetCode/MultiplyStrings.java
+++ src/com/dilin/java/practice/myLeetCode/MultiplyStrings.java
@@ -2,33 +2,25 @@ package com.dilin.java.practice.myLeetCode;
 
 public class MultiplyStrings {
 	public String multiply(String num1, String num2) {
+        //must calculate from the last digits
         int len1 = num1.length(), len2 = num2.length();
-        char[] number1 = num1.toCharArray(), number2 = num2.toCharArray();
-        int[] res = new int[len1 + len2 + 1];
-        int len = res.length;
+        char[] n1 = num1.toCharArray(), n2 = num2.toCharArray();
+        int[] n = new int[len1 + len2];
+        StringBuffer sb = new StringBuffer();
         for (int i = len1 - 1; i >= 0; i--) {
             for (int j = len2 - 1; j >= 0; j--) {
-                int prod = (number1[i] - '0') * (number2[j] - '0');
-                res[i + j + 2] += prod;
-                res[i + j + 1] += res[i + j + 2] / 10;
-                res[i + j + 2] %= 10;
+                int curt = (n1[i] - '0') * (n2[j] - '0');
+                n[i + j + 1] += curt;
+                n[i + j] += n[i + j + 1] / 10;
+                n[i + j + 1] %= 10;
             }
-        }
-        if (len1 != 0 || len2 != 0) {
-            res[0] += res[1] / 10;
-            res[1] %= 10;
         }
         int i = 0;
-        for (; i < len; i++) {
-            if (res[i] != 0) {
-                break;
-            }
+        while (i < len1 + len2 && n[i] == 0) i++;
+        while (i < len1 + len2) {
+            sb.append(n[i]);
+            i++;
         }
-        if (i == len) return "0";
-        StringBuffer sb = new StringBuffer();
-        for (; i < len; i++) {
-            sb.append(res[i]);
-        }
-        return sb.toString();
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
